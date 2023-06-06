b0a6fab1265eca4f059fc882ae95c12ae615d336
Oscar Alva <oscaraldrin23@gmail.com>
2023-06-06 16:25:15 +0200

    modificaciones de reduxtookit

--- src/services/store.js
+++ src/services/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
+import { articleApi } from './article';
 
 export const store = configureStore({
-    reducer: {}, //nos permite obtener una porción del estado global para reducir todo el estado para tomar solo lo que necesitamos.
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+    reducer: {[articleApi.reducerPath]: articleApi.reducer}, //nos permite obtener una porción del estado global para reducir todo el estado para tomar solo lo que necesitamos.
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)     ,
 });
